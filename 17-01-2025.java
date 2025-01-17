abe701fd98fea28a0be7a0938219f42a00f43e01
Rishabh Giri <giririshabh98@gmail.com>
2025-01-17 23:00:44 +0530

    Create 17-01-2025.java

---
+++ 17-01-2025.java
@@ -0,0 +1,30 @@
+// 2683. Neighboring Bitwise XOR
+
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        int n=derived.length;
+        int sum=0;
+
+        
+        // Hints-02: The xor-sum of the derived array should be 0 since there is always a duplicate occurrence of each element.
+
+        // Example: Length of derived=4
+        //     derived[0]=org[0]^org[1]
+        //     derived[1]=org[1]^org[2]
+        //     derived[2]=org[2]^org[3]
+        //     derived[3]=org[3]^org[0]
+
+        //     Xor-Sum = derived[0] ^ derived[1] ^ derived[2] ^ derived[3]
+        //     Xor-Sum = (org[0]^org[1]) ^ (org[1]^org[2]) ^ (org[2]^org[3]) ^ (org[3]^org[0])
+        //     //re-arrange it.....
+        //     Xor-Sum = (org[0]^org[0]) ^ (org[1]^org[1]) ^ (org[2]^org[2]) ^ (org[3]^org[3])
+        //     Xor-Sum = 0
+        
+        
+        for(int i=0; i<n; i++){
+            sum^=derived[i];
+        }
+
+        return sum==0;
+    }
+}
