a495abbd8f7900bfc8958936150a9f128bcf2ab2
Rishabh Giri <giririshabh98@gmail.com>
2025-01-27 22:24:24 +0530

    Create 27-01-2025.java

---
+++ 27-01-2025.java
@@ -0,0 +1,41 @@
+// 1462. Course Schedule IV
+
+class Solution {
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        int n = prerequisites.length, m = queries.length;
+        if(n == 0){
+            List<Boolean> res = new ArrayList<>(m);
+            for(int i=0; i<m; i++) res.add(false);
+            return res;
+        }
+        List<Integer> graph[] = new ArrayList[numCourses];
+        for(int i=0; i<numCourses; i++) graph[i] = new ArrayList<>();
+        for(int p[] : prerequisites){
+            graph[p[0]].add(p[1]);
+        }
+        boolean isReachable[][] = new boolean[numCourses][numCourses];
+        for(int i=0; i<numCourses; i++){ 
+            if(!isReachable[i][i]){ // doing dfs from every node
+                dfs(i, graph, isReachable);
+            }
+        }
+        List<Boolean> res = new ArrayList<>(m);
+        for(int q[] : queries){
+            res.add(isReachable[q[0]][q[1]]);
+        }
+        return res;
+    }
+    private void dfs(int curr, List<Integer> graph[], boolean isReachable[][]){
+        isReachable[curr][curr] = true;
+        for(int neigbor : graph[curr]){
+            if(!isReachable[curr][neigbor]){
+                isReachable[curr][neigbor] = true;
+                dfs(neigbor, graph, isReachable);
+                for(int i=0; i<isReachable.length; i++){
+                    isReachable[curr][i] |= isReachable[neigbor][i]; // all the nodes
+                    // which are reachable from neigbor are reachable from curr as well.
+                }
+            }
+        }
+    }
+}
