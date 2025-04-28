f9eec8e71fc2774a99db074aca80478a5e080f24
Rishabh Giri <giririshabh98@gmail.com>
2025-04-28 14:07:10 +0530

    Create 28-04-2025.java

---
+++ 28-04-2025.java
@@ -0,0 +1,22 @@
+2302. Count Subarrays With Score Less Than K
+
+class Solution {
+    public long countSubarrays(int[] nums, long k) {
+        
+        long total =0 ;
+        long count=0;
+        for (int start=0,end =0 ; end <nums.length;end++){
+
+            total += nums[end];
+
+            while (start<=end && total*(end-start+1)>=k){
+                total -=nums[start];
+                start++;
+            }
+            count += end -start+1;
+
+        }
+        return count;
+
+    }
+}
