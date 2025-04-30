66958fae12cc85323761bd784bebc745b731cdb9
Rishabh Giri <giririshabh98@gmail.com>
2025-04-30 09:37:38 +0530

    Create 30-04-2025.java

---
+++ 30-04-2025.java
@@ -0,0 +1,13 @@
+1295. Find Numbers with Even Number of Digits
+
+class Solution {
+    public int findNumbers(int[] nums) {
+        int result = 0;
+        for (int number: nums) {
+            if ((number > 9 && number < 100) || 
+            (number > 999 && number < 10000) || 
+            (number == 100000)) result++;
+        }
+        return result;
+    }
+}
