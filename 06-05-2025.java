402ad4fecac896320041efcb76b793c17b0bd8e1
Rishabh Giri <giririshabh98@gmail.com>
2025-05-06 10:36:43 +0530

    Create 06-05-2025.java

---
+++ 06-05-2025.java
@@ -0,0 +1,16 @@
+1920. Build Array from Permutation
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        apermutation(nums,0);
+        return nums;
+    }
+    void apermutation(int[] nums, int start){
+        if(start<nums.length){
+            int temp = nums[start];
+            int result = nums[temp];
+            apermutation(nums,start+1);
+            nums[start]=result;
+        }
+    }
+}
