8541d4ebf3dd8a8fa5e4de5cfb58df43b63d102d
Rishabh Giri <giririshabh98@gmail.com>
2025-05-23 22:48:32 +0530

    Create 23-05-2025.java

---
+++ 23-05-2025.java
@@ -0,0 +1,15 @@
+3068. Find the Maximum Sum of Node Values
+
+class Solution {
+     public long maximumValueSum(int[] A, int k, int[][] edges) {
+        long res = 0;
+        int d = 1 << 30, c = 0;
+        for (int a : A) {
+            int b = a ^ k;
+            res += Math.max(a, b);
+            c ^= a < b ? 1 : 0;
+            d = Math.min(d, Math.abs(a - b));
+        }
+        return res - d * c;
+    }
+}
