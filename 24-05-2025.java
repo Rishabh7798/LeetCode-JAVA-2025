5381e3a9f47eedff308682bd2b9de6803435d066
Rishabh Giri <giririshabh98@gmail.com>
2025-05-24 10:32:44 +0530

    Create 24-05-2025.java

---
+++ 24-05-2025.java
@@ -0,0 +1,14 @@
+2942. Find Words Containing Character
+
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> resultIndices = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) {
+                resultIndices.add(i);
+            }
+        }
+        return resultIndices;
+        
+    }
+}
